58c16ee70e81f632f5a0a6a3ec0ad5abeefa694f
Avalon96 <157373795+Avalon96@users.noreply.github.com>
2024-01-25 02:21:44 +0300

    Tutorial 1
    Finished C# tutorial 1 in freeCodeCamp

---
+++ Hello World/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.WriteLine("You wrote your first lines of code.");
+Console.WriteLine();
+
+// Write code that produces the following output:
+// Output
+// This is the first line.
+// This is the second line.
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
+Console.WriteLine();
