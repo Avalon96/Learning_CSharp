46e98b7e88324d41477ef57659d8e6152b7b9995
Avalon96 <157373795+Avalon96@users.noreply.github.com>
2024-01-25 13:34:15 +0300

    Edited Comments

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -4,10 +4,13 @@ Console.Write(" ");
 Console.WriteLine("You wrote your first lines of code.");
 Console.WriteLine();
 
-// Write code that produces the following output:
-// Output
-// This is the first line.
-// This is the second line.
+/*
+#Exercise
+
+Write code that produces the following output:
+This is the first line.
+This is the second line.
+*/
 Console.WriteLine("This is the first line.");
 Console.Write("This is the second line.");
 Console.WriteLine();
