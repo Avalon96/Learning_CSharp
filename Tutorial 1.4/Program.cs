08edc9510a7cfd84bb8f10f33ee145cb00c69d44
Avalon96 <157373795+Avalon96@users.noreply.github.com>
2024-01-28 19:52:08 +0300

    Create Program.cs

---
+++ Tutorial 1.4/Program.cs
@@ -0,0 +1,15 @@
+﻿int value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
+
+// Fahrenheit to Celsius
+// (F - 32) * 5 / 9 = C
+int fahrenheit = 94;
+float celsius = (float)(fahrenheit - 32) * 5 / 9;
+
+Console.WriteLine("The temprature is " + celsius + " Celsius.");
+
+Console.WriteLine("Windows " + 1 + 1);  // Outputs Windows 11, not Windows 2
